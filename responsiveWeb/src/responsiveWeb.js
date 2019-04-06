3e4ff4da5c4a2dc62deb815e93bddbd452123f95
Javaughn Burke <javaughnburke@Javaughns-MacBook-Air.local>
2019-04-06 11:33:55 -0400

    web design

---
+++ responsiveWeb/src/responsiveWeb.js
@@ -0,0 +1,40 @@
+jQuery(document).ready(function () {
+    "use strict"
+
+    $('#slid-carousel').carouFredSel({
+        responsive: true,
+        width: '100%',
+        circular: true,
+        scroll: {
+            items: 1,
+            duration: 500,
+            pauseOnHover: true
+        },
+        auto: true,
+        items: {
+            visible: {
+                min: 1,
+                max: 1
+            },
+            height: "variable"
+        },
+        pagination: {
+            container: ".sliderpager",
+            pageAnchorBuilder: false
+        }
+
+    });
+    $(Window).scroll(function () {
+        var start = $(window).scrollTop();
+        if(start >= 60) {
+            $("header").addClass('secondary');
+        }
+        else { 
+            if($("header").hasClass('secondary')) {
+                $("header").removeClass('secondary')
+            }
+        }
+        
+    });
+});
+
